e30aead0cd42f374b4e8f237cc15b75712760caf
reilly-freret <reilly.freret@gmail.com>
2018-11-08 17:53:44 -0600

    commit early and often

---
+++ Pulse/Pulse/controllers/LogInController.swift
@@ -0,0 +1,14 @@
+//
+//  LogInController.swift
+//  Pulse
+//
+//  Created by Reilly Freret on 11/8/18.
+//  Copyright © 2018 Reilly Freret. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogInController: UIViewController {
+    
+}
---
+++ Pulse/Pulse/controllers/SignUpController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpController.swift
+//  Pulse
+//
+//  Created by Reilly Freret on 11/8/18.
+//  Copyright © 2018 Reilly Freret. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
