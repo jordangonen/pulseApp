a55deccde521606aeee52ac3576ed7b2b3c3243b
reilly-freret <reilly.freret@gmail.com>
2018-11-08 16:23:51 -0600

    xcode workspace created, firebase initialized

---
+++ Pulse/Pulse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Reilly Freret on 11/8/18.
+//  Copyright © 2018 Reilly Freret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
