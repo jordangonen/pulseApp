94f7096b0c173ab3f289790f25fe1198215129b1
jordangonen <jordangonen@wustl.edu>
2018-12-02 16:13:05 -0600

    removed delete - ironyyy

--- Pulse/Pulse/controllers/SettingsController.swift
+++ Pulse/Pulse/controllers/SettingsController.swift
@@ -78,48 +78,7 @@ class SettingsController: UIViewController {
     }
 
  
-    @IBAction func deleteUser(_ sender: Any) {
-        var user = Auth.auth().currentUser;
-        var name: String;
-        var email: String;
-        email = (user?.email)!;
-
-        
-        
-        let alert = UIAlertController(title: "Delete Account", message: "This account will be deleted", preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "Delete", style: UIAlertActionStyle.default, handler: {(action:UIAlertAction!) in
-          //  Auth.auth().currentUser?.delete(completion: name);
-            Auth.auth().sendPasswordReset(withEmail: email);
-
-            
-            
-            do {
-                try Auth.auth().signOut()
-                self.view.screenLoading()
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    self.view.screenLoaded()
-                    let v = UINavigationController(rootViewController: UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loggedOut"))
-                    v.setNavigationBarHidden(true, animated: false)
-                    UIView.transition(with: ((UIApplication.shared.delegate?.window)!)!, duration: 0.5, options: .transitionFlipFromLeft, animations: {
-                        ((UIApplication.shared.delegate?.window)!)!.rootViewController = v
-                    }, completion: nil)
-                }
-            } catch let signOutError as NSError {
-                print ("Error signing out: %@", signOutError)
-            }
-            
-            
-            
-            
-            
-            
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: true)
-        
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
